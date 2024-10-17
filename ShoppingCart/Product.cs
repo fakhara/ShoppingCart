427a3765fd9b024bff54ba35524dc1878837f784
Fakhara <fakhara.imran82@gmail.com>
2024-10-17 11:40:30 +0200

    Add project files.

---
+++ ShoppingCart/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCart
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string PriceType { get; set; }
+
+        public Product(int productId, string name, decimal price, string priceType)
+        {
+            ProductId = productId;
+            Name = name;
+            Price = price;
+            PriceType = priceType;
+        }
+    }
+}
