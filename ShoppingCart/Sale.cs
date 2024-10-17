ea2d08f2e447106b62d2a4f6666d87038d578baa
Fakhara <fakhara.imran82@gmail.com>
2024-10-17 12:08:41 +0200

    Add Product and Sale class in Shopping cart

---
+++ ShoppingCart/Sale.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCart
+{
+    public class Sale
+    {
+        public List<(Product product, int quantity)> Items { get; private set; }
+
+        public Sale()
+        {
+            Items = new List<(Product product, int quantity)>(); 
+        }
+        public void AddProduct(Product product, int quantity)
+        {
+            Items.Add((product, quantity));
+        }
+        public decimal GetTotal()
+        {
+            return Items.Sum(item => item.product.Price * item.quantity);
+        }
+    }
+}
